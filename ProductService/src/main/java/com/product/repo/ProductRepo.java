4361248f36317ac61707ce690b3066e625164303
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-28 00:39:21 +0530

    TEest controller added'

--- ProductService/src/main/java/com/product/repo/ProductRepo.java
+++ ProductService/src/main/java/com/product/repo/ProductRepo.java
@@ -1,13 +1,11 @@
 package com.product.repo;
 
+import com.product.modal.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.product.modal.Product;
+public interface ProductRepo extends MongoRepository<Product, String> {
+    // findByName queries,
+    public Product findByname(String name);
 
-public interface ProductRepo extends MongoRepository<Product, String>
-{
-	// findByName queries,
-	public Product findByname(String name);
-	
 //	public Boolean isProductExistsByName(String name);
 }
--- ProductService/src/main/java/com/product/service/ProductService.java
+++ ProductService/src/main/java/com/product/service/ProductService.java
@@ -1,60 +1,50 @@
 package com.product.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.product.dto.ProductDTO;
 import com.product.dto.ProductResponse;
 import com.product.modal.Product;
 import com.product.repo.ProductRepo;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
-public class ProductService 
-{
-	// DI(constructor injection)
-	@Autowired
-	private ProductRepo productRepo;
-	
-	// INSERT INTO TABLE product VALUES();
-	public void createProduct(ProductResponse productResponse)
-	{
-		Product product = Product.builder()
-							.name(productResponse.getName())
-							.description(productResponse.getDescription())
-							.price(productResponse.getPrice())
-							.build();
-				
-				productRepo.save(product);
-				log.info("Product {} is saved",product.getId());	
-				
-	}
-	
-	// SELECT * FROM product;
-	public List<Product> getProducts()
-	{
-		List<Product> product = productRepo.findAll();
-		log.info("Products found in the database ");
-		return product;
-	}
-	
-	// SELECT * FROM product WHERE product.name=name;
-	public Product getProduct(String name)
-	{
-		return productRepo.findByname(name);
-	}
-	
-	// delete from table
-	public void deleteProduct()
-	{
-		productRepo.deleteAll();
-		log.info("Products deleted from the table,");
-	}
-	
-	
-	
+public class ProductService {
+    @Autowired
+    private ProductRepo productRepo;
+
+    // INSERT INTO TABLE product VALUES();
+    public void createProduct(ProductResponse productResponse) {
+        Product product = Product.builder()
+                .name(productResponse.getName())
+                .description(productResponse.getDescription())
+                .price(productResponse.getPrice())
+                .build();
+
+        productRepo.save(product);
+        log.info("Product {} is saved", product.getId());
+
+    }
+
+    // SELECT * FROM product;
+    public List<Product> getProducts() {
+        List<Product> product = productRepo.findAll();
+        log.info("Products found in the database ");
+        return product;
+    }
+
+    // SELECT * FROM product WHERE product.name=name;
+    public Product getProduct(String name) {
+        return productRepo.findByname(name);
+    }
+
+    // delete from table
+    public void deleteProduct() {
+        productRepo.deleteAll();
+        log.info("Products deleted from the table,");
+    }
+
+
 }
