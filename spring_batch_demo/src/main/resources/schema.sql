fc986ffe317ef8a36ff1e03833fbf8494961dfd8
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-03 07:24:02 +0530

    SQL script added

---
+++ spring_batch_demo/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS products (
+    product_id INTEGER PRIMARY KEY,
+    product_title VARCHAR(255),
+    product_description VARCHAR(255),
+    product_price VARCHAR(255),
+    product_discount VARCHAR(255),
+    product_discounted_amount VARCHAR(255)
+);
